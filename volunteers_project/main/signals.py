54265eae2893979e19699c22b4f08f95a4075ebf
Lifanna <solano.lifan2@bk.ru>
2024-03-11 20:54:59 +0600

    removed hosts

--- volunteers_project/main/signals.py
+++ volunteers_project/main/signals.py
@@ -7,12 +7,13 @@ from .models import Task
 
 @receiver(post_save, sender=Task)
 def send_payment_status_update(sender, instance, **kwargs):
-    if instance.assigned == False:
-        channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(
-            'task_group',
-            {
-                'type': 'task_status_create',
-                'task': instance,
-            }
-        )
+    pass
+    # if instance.assigned == False:
+    #     channel_layer = get_channel_layer()
+    #     async_to_sync(channel_layer.group_send)(
+    #         'task_group',
+    #         {
+    #             'type': 'task_status_create',
+    #             'task': instance,
+    #         }
+    #     )
